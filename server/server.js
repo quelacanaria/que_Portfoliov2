37e6a984b6c066c58a58f9022e3c28c1e4691bac
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-12-05 01:28:36 +0800

    commit

--- server/server.js
+++ server/server.js
@@ -4,87 +4,76 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const nodemailer = require('nodemailer');
-const { TransactionalEmailsApi } = require('@getbrevo/brevo');
+const { Resend } = require('resend');
 
-const login = process.env.LOGIN;
+const host = process.env.HOST;
 const PORT = process.env.PORT;
 const user = process.env.USER;
+const resend = new Resend(process.env.API);
 const userpassword = process.env.USERPASSWORD;
-const api = process.env.API;
 
-app.use(cors({ origin: 'https://que-portfoliov2-1.onrender.com' }));
+app.use(cors({origin: 'https://que-portfoliov2-1.onrender.com'}));    
 app.use(bodyParser.json());
 
-const brevo = new TransactionalEmailsApi();
-brevo.authentications.apiKey.apiKey = api;
-
-const transporter = nodemailer.createTransport({
-  host: 'smtp.relay.brevo.com',
-  port: 587,
-  secure: false,
-  auth: {
-    user: user,
-    pass: userpassword,
-  },
-});
-
 app.get('/', (req, res) => {
-  res.send('hello');
+    res.send('hello');
 });
 
 app.post('/api/post', async (req, res) => {
-  const { name, subject, email, phone, message } = req.body;
+    const {name, subject, email, phone, message} = req.body;
 
-  if (!name || !subject || !email || !phone || !message) {
-    return res.status(400).json({ error: 'all fields a required' });
-  }
+    if(!name || !subject || !email || !phone || !message){
+        return res.status(400).json({error: 'all fields a required'})
+    }
 
-  const adminHtml = `
-    <div style="font-family: Arial, sans-serif; padding: 20px; color: #333;">
-      <h2> ${subject}</h2>
-      <p><strong>Name:</strong> ${name}</p>
-      <p><strong>Email:</strong> ${email}</p>
-      <p><strong>Phone:</strong> ${phone}</p>
-      <p><strong>Message:</strong></p>
-      <p>${message}</p>
-    </div>
-  `;
+const adminHtml = `
+  <div style="font-family: Arial, sans-serif; padding: 20px; color: #333;">
+    <h2> ${subject}</h2>
+    <p><strong>Name:</strong> ${name}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Phone:</strong> ${phone}</p>
+    <p><strong>Message:</strong></p>
+    <p>${message}</p>
+  </div>
+`;
 
-  const userHtml = `
-    <div style="font-family: Arial, sans-serif; padding: 20px; color: #333;">
-      <h2>Thanks for Reaching Out!</h2>
-      <p>Hi ${name},</p>
-      <p>Thank you for contacting me. I’ve received your message and I’ll get back to you as soon as possible.</p>
-      <p>Looking forward to connecting with you!</p>
-      <p>Best regards, </p>
-      <p>Quekeneth S. Lacanaria</p>
-    </div>
-  `;
 
-  try {
-    // Email to admin
-    await brevo.sendTransacEmail({
-      sender:  login ,
-      to:  user ,
-      subject: `${subject}`,
-      htmlContent: adminHtml, // <-- changed here
-    });
+const userHtml = `
+  <div style="font-family: Arial, sans-serif; padding: 20px; color: #333;">
+    <h2>Thanks for Reaching Out!</h2>
+    <p>Hi ${name},</p>
+    <p>Thank you for contacting me. I’ve received your message and I’ll get back to you as soon as possible.</p>
+    <p>Looking forward to connecting with you!</p>
+    <p>Best regards, </p>
+    <p>Quekeneth S. Lacanaria</p>
+  </div>
+`;
 
-    // Confirmation email to user
-    await brevo.sendTransacEmail({
-      sender:  login ,
-      to:email,
-      subject: 'Thanks for Reaching out',
-      htmlContent: userHtml, // <-- changed here
-    });
+try{
+  await resend.emails.send({
+    from: 'onboarding@resend.dev',
+    to: user,
+    subject: `${subject}`,
+    html: adminHtml
+  });
 
-    res.status(200).json({ success: true, message: 'Email sent successfully!' });
-  } catch (error) {
-    console.log('error -> ', error);
-    res.status(500).json({ success: false, message: 'Something went wrong' });
-  }
-});
+  await resend.emails.send({
+    from: 'onboarding@resend.dev',
+    to: email,
+    subject: "Thanks for Reaching out",
+    html: userHtml
+  });
+
+  res.status(200).json({success: true, message: 'Email sent successfully!'})
+
+}catch(error){
+  console.log('error -> ', error);
+  res.status(500).json({success: false, message: 'Something went wrong'});
+}
+
+})
+
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`Server is running on port ${PORT}`);
+})
